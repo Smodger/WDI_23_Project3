37603ac2247eea12ddd5527e6314da9d4b32bd3f
Lawrie Platt McWall <lawrie.pm91@gmail.com>
2016-11-16 15:44:20 +0000

    controllers and routes added

--- models/user_images.js
+++ models/user_images.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 const userImages = new mongoose.Schema({
-  images: [
-    { objectId:  }
-  ]
+  image: { type: String },
+  caption: { type: String },
+  projectId: { type: String },
+  location: { lat: Number, lng: Number, name: String }
 });
 
 module.exports = mongoose.model('userImages', userImages);
