960f02117031e9d8c6d05032311fa15b105ed98b
Smodger <chrisnorman1988@hotmail.co.uk>
2016-11-21 14:52:40 +0000

    added googleMaps to challengeShow page

---
+++ src/js/directives/googleMap.js
@@ -0,0 +1,33 @@
+angular.module('goApp')
+  .directive('googleMap', googleMap);
+
+googleMap.$inject = ['$window', 'Challenge'];
+
+function googleMap($window, Challenge) {
+  return {
+    restrict: 'E',
+    replace: true,
+    template: '<div class="google-map">Google Map HERE</div>',
+    scope: {
+      challenge: '='
+    },
+    link: function(scope, element) {
+      scope.$watch('challenge', () => {
+        if(scope.challenge) {
+
+          const map = new $window.google.maps.Map(element[0], {
+            center: scope.challenge.location,
+            zoom: 12
+          });
+
+          new $window.google.maps.Marker({
+            position: scope.challenge.location,
+            map: map,
+            animation: $window.google.maps.Animation.DROP
+          });
+        }
+      });
+
+    }
+  };
+}
