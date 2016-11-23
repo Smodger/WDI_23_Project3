6333d1892184a7f41c27a0d5c5373e788db07644
Smodger <chrisnorman1988@hotmail.co.uk>
2016-11-23 10:06:13 +0000

    updated seeds and added seedsStories

--- db/seeds.js
+++ db/seeds.js
@@ -123,8 +123,7 @@ user2.save((err, user2) => {
   });
 });
 
-//USER 3 CN FOR SEEDS AMALGAMATION
-
+//Additional
 let user3ID;
 const user3 = new User({
   username: 'Chris Norman',
@@ -147,66 +146,100 @@ user3.save((err, user3) => {
   console.log(user3 + ' Was created!');
   user2ID = user3._id;
   console.log('User3 id is: ', user3ID);
+});
 
-  let challenge3ID;
-  const challenge3 = new Challenge({
-    name: 'The HRP',
-    startDate: '01/06/17',
-    finishDate: '01/08/17',
-    cost: '£2000',
-    location: { lat: 42.6929641, lng: -1.7960056 },
-    description: 'Walk from the Atlantic Coast to the Mediterranean Coast across they Pyrenees',
-    image:'https://scontent.flhr2-2.fna.fbcdn.net/v/t1.0-9/11013614_10204617239103483_1541173441935147825_n.jpg?oh=d1080e1de8c698b52d2a94e87fd5aab6&oe=58BDED79',
-    video: '<iframe width="640" height="360" src="https://www.youtube.com/embed/6vn0Er8wJIE" frameborder="0" allowfullscreen></iframe>',
-    like: [],
-    projectCreator: [user2ID],
-    comments: 'comment 1',
-    participants: [user2ID]
-  });
+let user4ID;
 
-  challenge3.save((err, challenge3) => {
-    if(err) return console.log(err);
-    // console.log('running?');
-    challenge2ID = challenge3._id;
-    console.log('challenge3ID: ',challenge3ID);
-    console.log('Created ', challenge3);
-  });
+const user4 = new User({
+  username: 'liam-fitzpatrick',
+  email: 'liam-fitzpatrick@gmail.com',
+  password: 'password',
+  passwordConfirmation: 'password',
+  dob: '03/02/1970',
+  gender: 'male',
+  strapline: 'love climbing!!',
+  bio: 'Climbing enthusiast and photographer',
+  coverPhoto: 'http://activeazur.wpengine.netdna-cdn.com/wp-content/uploads/2015/02/rock-climbing-south-of-france-730x230.jpg',
+  profilePhoto: 'http://dmmclimbing.com/wp-content/uploads/2011/05/Liam-on-By-the-Power-of-RAAA-7C-St-Bees.jpg',
+  // activeChallenges: [], //Challenge
+  video: '<iframe width="560" height="315" src="https://www.youtube.com/watch?v=-8qvasMlYXo" frameborder="0" allowfullscreen></iframe>',
+  like: [], //User
+  locked: false
+});
 
-  user id : 'XXX'
-  challengeid: 'XXX'
-  entries: [{
-    title: 'Haute Route Pyrenees - Stage 1'
-    shortintro: 'Stage 1 - Hendaye to Lescun'
-    mainContent: 'Start of a long walk'
-    photo: 'https://scontent.flhr2-2.fna.fbcdn.net/v/t1.0-0/p370x247/11753287_10206148422102151_1234116927840207915_n.jpg?oh=e7cd9c160c52b8a02607a0fcb92b06a4&oe=58BD7A6A'
-    caption: 'Day 1 on the beach at Hendaye'
-    order: 1
-  }, {
-    title: 'Haute Route Pyrenees - Stage 2'
-    shortintro: 'Stage 2 - Lescun to Gavarnie'
-    mainContent: 'Start entering the higher mountains'
-    photo: 'https://scontent.flhr2-2.fna.fbcdn.net/v/t1.0-9/11825906_10204567008567751_3926431220038040428_n.jpg?oh=87e3aba0efeb59c0bfca8636353a4e59&oe=5886FAD6'
-    caption: 'Reach the snowline earlier than expected'
-    order: 2
-  }, {
-    title: 'Haute Route Pyrenees - Stage 3'
-    shortintro: 'Stage 3 - Gavarnie to Salardu'
-    mainContent: 'Craving Coke and chocolate'
-    photo: 'https://scontent.flhr2-2.fna.fbcdn.net/v/t1.0-9/11836630_10204617241263537_4930515482094797742_n.jpg?oh=4a36a1460a5a62b2159f3f47b9318b50&oe=58C12D2F'
-    caption: 'If Carlsberg did campsites'
-    order: 3
-  }, {
-    title: 'Haute Route Pyrenees - Stage 4'
-    shortintro: 'Stage 4 - Salardu to Andorra Viehla'
-    mainContent: 'Fell and nearly died'
-    photo: 'https://scontent.flhr2-2.fna.fbcdn.net/v/t1.0-9/11951838_10204712835093323_9016186575097653050_n.jpg?oh=f3eb49da26d83ebd1f4e75288d261437&oe=58D0B656'
-    caption: 'Lovely cloud inversion'
-    order: 4
-  }, {
-    title: 'Haute Route Pyrenees - Stage 5'
-    shortintro: 'Stage 5 - Andorra Viehla to Banyuls-sur-Mere'
-    mainContent: ''
-    photo: 'https://scontent.flhr2-2.fna.fbcdn.net/v/t1.0-9/11954682_10204712846813616_9082390366798155313_n.jpg?oh=55badce016a0957d92ac69f253200d59&oe=58BD7DA8'
-    caption: 'Bath time for wild horses'
-    order: 5
-  }
+user4.save((err, user4) => {
+  if(err) return console.log(err);
+  console.log(user4 + ' Was created!');
+  user4ID = user4._id;
+  console.log('user4 id is :', user1ID);
+});
+
+const userLuke = new User({
+  username: 'Luke Hammond',
+  email: 'luke@example.com',
+  password: 'password',
+  passwordConfirmation: 'password',
+  dob: '13/09/1991',
+  gender: 'male',
+  strapline: 'Amatuer skier',
+  bio: 'I fall... a lot',
+  profilePhoto: 'https://scontent-cdg2-1.cdninstagram.com/t51.2885-19/s320x320/14145461_601713173321653_551387777_a.jpg',
+  video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/48lpr0K54DM" frameborder="0" allowfullscreen></iframe>',
+  like: [], //User
+  locked: false
+});
+
+userLuke.save((err, userLuke) => {
+  if(err) return console.log(err);
+  console.log(user1 + ' Was created!');
+  const userLukeID = userLuke._id;
+  console.log('user1 id is :', userLukeID);
+});
+
+let challenge3ID;
+const challenge3 = new Challenge({
+  name: 'The HRP',
+  startDate: '01/06/17',
+  finishDate: '01/08/17',
+  cost: '£2000',
+  location: { lat: 42.6929641, lng: -1.7960056 },
+  description: 'Walk from the Atlantic Coast to the Mediterranean Coast across they Pyrenees',
+  image: 'https://scontent.flhr2-2.fna.fbcdn.net/v/t1.0-9/11013614_10204617239103483_1541173441935147825_n.jpg?oh=d1080e1de8c698b52d2a94e87fd5aab6&oe=58BDED79',
+  video: '<iframe width="640" height="360" src="https://www.youtube.com/embed/6vn0Er8wJIE" frameborder="0" allowfullscreen></iframe>',
+  like: [],
+  projectCreator: [user3ID],
+  comments: '',
+  participants: [user3ID]
+});
+
+challenge3.save((err, challenge3) => {
+  if(err) return console.log(err);
+  // console.log('running?');
+  challenge3ID = challenge3._id;
+  console.log('challenge3ID: ',challenge3ID);
+  console.log('Created ', challenge3);
+});
+
+let challenge4ID;
+const challenge4 = new Challenge({
+  name: 'Walk the Pennine Way',
+  startDate: '01/06/18',
+  finishDate: '20/07/18',
+  cost: '£1000',
+  location: { lat: 55.5475, lng: 2.2739 },
+  description: 'Steeped in history, this National Trail chases along the mountain tops along the rugged backbone of England and offers 268 miles of the finest upland walking in England. A once in a lifetime experience. ',
+  image: 'https://www.sherpavan.com/accomm_booking/images/PW.gif',
+  video: 'video',
+  like: [],
+  projectCreator: [user4ID],
+  comments: 'Not for the feint hearted',
+  participants: [user4ID]
+});
+
+challenge4.save((err, challenge4) => {
+  if(err) return console.log(err);
+  // console.log('running?');
+  challenge4ID = challenge4._id;
+  console.log('challenge3ID: ',challenge4ID);
+  console.log('Created ', challenge4);
+});
